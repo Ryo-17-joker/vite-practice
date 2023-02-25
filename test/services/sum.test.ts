ad40f22608f1845b7288b4f2a8315a9c4bc57827
Ryo-KM <king.and.joker00@gmail.com>
2023-02-25 15:48:53 +0900

    testコードを修正した

--- test/services/sum.test.ts
+++ test/services/sum.test.ts
@@ -1,10 +1,6 @@
+import { sum } from "../../src/services/sum"
 
-// import { sum } from "../../src/services/sum"
+it('adds 1 + 2 to equal 3', () => {
+  expect(sum(1,2)).toBe(3);
+})
 
-// it('adds 1 + 2 to equal 3', () => {
-//   expect(sum(1,2)).toBe(3);
-// })
-
-it("check", () => {
-  console.log("OK");
-});
